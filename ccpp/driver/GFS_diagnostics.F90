faa09b19763b173dc32852b6ca76bfecf6d17bde
Johana Romero-Alvarez <johana.romero-alvarez@noaa.gov>
2025-10-02 23:10:31 -0400

    fixing typo smokem6_RRFS for smokedc6_RRFS

--- ccpp/driver/GFS_diagnostics.F90
+++ ccpp/driver/GFS_diagnostics.F90
@@ -4821,7 +4821,7 @@ module GFS_diagnostics
       allocate (ExtDiag(idx)%data(nblks))
       do nb = 1,nblks
        !ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smoke2d_RRFS(:,3)
-       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokem6_RRFS(:,1,3)
+       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokedc6_RRFS(:,1,3)
       enddo
 
       endif
@@ -4873,7 +4873,7 @@ module GFS_diagnostics
       allocate (ExtDiag(idx)%data(nblks))
       do nb = 1,nblks
        !ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smoke2d_RRFS(:,1)
-       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokem6_RRFS(:,2,5)
+       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokedcm6_RRFS(:,2,5)
       enddo
 
       idx = idx + 1
@@ -4885,7 +4885,7 @@ module GFS_diagnostics
       allocate (ExtDiag(idx)%data(nblks))
       do nb = 1,nblks
        !ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smoke2d_RRFS(:,2)
-       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokem6_RRFS(:,1,5)
+       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokedc6_RRFS(:,1,5)
       enddo
 
 
@@ -4898,7 +4898,7 @@ module GFS_diagnostics
       allocate (ExtDiag(idx)%data(nblks))
       do nb = 1,nblks
        !ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smoke2d_RRFS(:,4)
-       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokem6_RRFS(:,4,5)
+       ExtDiag(idx)%data(nb)%var2 => Sfcprop(nb)%smokedc6_RRFS(:,4,5)
       enddo
 
       endif smoke_forecast_mode
